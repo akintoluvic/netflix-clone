2e63ec257ddb206787ec3920a64353381fe11e4c
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-07-16 13:16:35 +0100

    getting movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Row from './Row'
+import requests from './requests'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Lets Start</h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "./axios";
 
-function Row() {
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([])
+    
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+        // return () => {
+        //     cleanup
+        // }
+    }, [fetchUrl])
+
+    console.log(movies)
+
     return (
         <div>
-            
+            <h2>{title}</h2>
+            <div className="row-posters">
+                
+            </div>
+
         </div>
     )
 }
