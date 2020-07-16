de08f3c53bb3ac16c7a2c3abc89b01439de9219b
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-07-16 16:10:40 +0100

    nav working

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import netflix from "./netflix.png";
 import smi from "./smi.png";
 import "./nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav-black"}`}>
       <img className="nav-logo" src={netflix} alt="Netflix logo" />
       <img className="nav-avatar" src={smi} alt="Netflix logo" />
     </div>
